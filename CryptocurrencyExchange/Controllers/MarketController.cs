57dd0687f5c2b231253fd3325292dc540fceb271
supalapka <yurabalint0103@gmail.com>
2023-04-28 13:48:56 +0300

    added demo market controller

---
+++ CryptocurrencyExchange/Controllers/MarketController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace CryptocurrencyExchange.Controllers
+{
+    [Route("market")]
+    public class MarketController : Controller
+    {
+        [HttpGet("list")]
+        public IActionResult GetSymbolsByPage() //composite to service later
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://api.binance.com/api/v3/");
+            HttpResponseMessage response = client.GetAsync("ticker/24hr").Result;
+            string responseBody = response.Content.ReadAsStringAsync().Result;
+
+            List<string> symbols = new List<string>();
+
+            JArray marketData = JArray.Parse(responseBody);
+            foreach(JObject market in marketData)
+            {
+                symbols.Add(market["symbol"].ToString());
+            }
+
+            symbols = symbols.Where(x => x.Contains("USDT")).Take(100).ToList();
+            return Ok(symbols);
+        }
+       
+    }
+}
+
+
