0c53420f39007e067bd7a40151dcafbb48a313e1
supalapka <yurabalint0103@gmail.com>
2023-05-01 15:11:03 +0300

    Added db row locking to prevent concurrent modification of user data.

--- CryptocurrencyExchange/Services/FuturesService.cs
+++ CryptocurrencyExchange/Services/FuturesService.cs
@@ -16,24 +16,32 @@ namespace CryptocurrencyExchange.Services
 
         public async Task CreateFutureAsync(FutureDto futureDto, int userId)
         {
-            var userUsdt = await _dataContext.WalletItems.Where(x => x.UserId == userId
-            && x.Symbol == "usdt").FirstAsync();
-            if (userUsdt.Amount < (double)futureDto.Margin)
-                throw new Exception("Not anough balance in usdt to start future position");
+            using var transaction = await _dataContext.Database.BeginTransactionAsync(System.Data.IsolationLevel.Serializable);
+            await _dataContext.Database.ExecuteSqlRawAsync($"SELECT * FROM WalletItems WITH (TABLOCKX) WHERE UserId = {userId} AND Symbol = 'usdt'");;
 
-            var future = new Future();
-            future.Symbol = futureDto.Symbol;
-            future.EntryPrice = (double)futureDto.EntryPrice;
-            future.Margin = futureDto.Margin;
-            future.UserId = userId;
-            future.IsCompleted = false;
-            future.Leverage = futureDto.Leverage;
-            future.Position = futureDto.Position;
+            var userUsdt = await _dataContext.WalletItems
+                        .Where(x => x.UserId == userId && x.Symbol == "usdt")
+                        .FirstAsync();
 
-            userUsdt.Amount -= (double)futureDto.Margin;
+                    if (userUsdt.Amount < (double)futureDto.Margin)
+                    {
+                        throw new Exception("Not enough balance in usdt to start future position");
+                    }
 
-            await _dataContext.Futures.AddAsync(future);
-            await _dataContext.SaveChangesAsync();
+                    var future = new Future();
+                    future.Symbol = futureDto.Symbol;
+                    future.EntryPrice = (double)futureDto.EntryPrice;
+                    future.Margin = futureDto.Margin;
+                    future.UserId = userId;
+                    future.IsCompleted = false;
+                    future.Leverage = futureDto.Leverage;
+                    future.Position = futureDto.Position;
+
+                    userUsdt.Amount -= (double)futureDto.Margin;
+
+                     _dataContext.Futures.Add(future);
+                     _dataContext.SaveChanges();
+                     await transaction.CommitAsync();
         }
 
 
@@ -56,7 +64,7 @@ namespace CryptocurrencyExchange.Services
                 };
                 result.Add(futureDto);
             }
-
+            result.Reverse();
             return result;
         }
 
@@ -84,22 +92,31 @@ namespace CryptocurrencyExchange.Services
             var position = _dataContext.Futures.Find(id);
             position.IsCompleted = true;
 
-            var futureHistiry = new FutureHistory()
+            using var transaction = await _dataContext.Database.BeginTransactionAsync(System.Data.IsolationLevel.Serializable);
+            await _dataContext.Database.ExecuteSqlRawAsync($"SELECT * FROM WalletItems WITH (TABLOCKX) WHERE UserId = {position.UserId} AND Symbol = 'usdt'"); ;
+
+            var futureHistory = new FutureHistory()
             {
                 FutureId = position.Id,
-                Id = position.Id, //futureHistiry.id must be position.id 
+                Id = position.Id,
                 IsLiquidated = false,
                 MarkPrice = markPrice,
             };
-            await _dataContext.FutureHistory.AddAsync(futureHistiry);
 
-            var userUsdt = _dataContext.WalletItems.Where(x => x.UserId == position.UserId
-            && x.Symbol == "usdt").First();
+            await _dataContext.FutureHistory.AddAsync(futureHistory);
+
+            var userUsdt = _dataContext.WalletItems
+                .Where(x => x.UserId == position.UserId && x.Symbol == "usdt")
+                .First();
+
             userUsdt.Amount += (double)position.Margin;
             userUsdt.Amount += pnl;
 
             await _dataContext.SaveChangesAsync();
+            await transaction.CommitAsync();
+
         }
+
 
         public List<FutureHIstoryOutput> GetHistory(int userId)
         {
