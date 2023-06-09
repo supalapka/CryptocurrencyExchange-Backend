52c80f8ca0bb756a5c1ed1c49d892aa574a414e8
supalapka <yurabalint0103@gmail.com>
2023-06-10 00:47:38 +0300

    Created Exchange.Tests project

---
+++ CryptocurrencyExchange.Tests/ServicesTests/WalletServiceTests.cs
@@ -0,0 +1,15 @@
+﻿using CryptocurrencyExchange.Data;
+using CryptocurrencyExchange.Models;
+using CryptocurrencyExchange.Services;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using NUnit.Framework;
+
+namespace CryptocurrencyExchange.Tests.ServicesTests
+{
+    [TestFixture]
+    public class WalletServiceTests
+    {
+      
+    }
+}
