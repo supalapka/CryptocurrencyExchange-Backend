7eee16c56a3a44487d16c1cf0b3e8ea79ea7a525
Yuriy Balint <46222135+supalapka@users.noreply.github.com>
2026-02-08 20:04:56 +0700

    refactor: improve readability of WalletDomainServiceTests

--- CryptocurrencyExchange.Tests/Domain/WalletDomainServiceTests.cs
+++ CryptocurrencyExchange.Tests/Domain/WalletDomainServiceTests.cs
@@ -8,33 +8,26 @@ namespace CryptocurrencyExchange.Tests.Domain
     {
         private WalletDomainService walletDomainService = null!;
 
-        private const decimal BTC_PRICE = 20m;
-
         [SetUp]
         public void SetUp()
         {
             walletDomainService = new WalletDomainService();
         }
 
+
         [Test]
         public void Buy_WhenEnoughUsdt_ShouldDecreaseUsdtAndIncreaseCoin()
         {
             // Arrange
-            decimal initialUsdtAmount = 100;
-            decimal usdtToSpend = 20;
-
-            decimal expectedUsdtAfterBuy = 80;
-            decimal expectedCoinAmount = 1;
-
-            var usdt = WalletItemMother.CreateUsdt(amount: initialUsdtAmount);
+            var usdt = WalletItemMother.CreateUsdt(amount: 100);
             var btc = WalletItemMother.CreateBtc(amount: 0);
 
             // Act
-            walletDomainService.Buy(usdt, btc, usdtToSpend, BTC_PRICE);
+            walletDomainService.Buy(usdt, btc, usd: 20, coinPrice: 20);
 
             // Assert
-            Assert.AreEqual(expectedUsdtAfterBuy, usdt.Amount);
-            Assert.AreEqual(expectedCoinAmount, btc.Amount);
+            Assert.AreEqual(80, usdt.Amount);
+            Assert.AreEqual(1, btc.Amount);
         }
 
 
@@ -42,50 +35,39 @@ namespace CryptocurrencyExchange.Tests.Domain
         public void Buy_WhenUsdtIsInsufficient_ShouldThrowInsufficientFundsException()
         {
             // Arrange
-            decimal usdtToSpend = 100;
-
-            var usdt = WalletItemMother.CreateUsdt(amount: 50);
+            var usdt = WalletItemMother.CreateUsdt(amount: 0);
             var btc = WalletItemMother.CreateBtc(amount: 0);
 
             // Act and Assert
             Assert.Throws<Exceptions.InsufficientFundsException>(() =>
-            walletDomainService.Buy(usdt, btc, usdtToSpend, BTC_PRICE));
+            walletDomainService.Buy(usdt, btc, usd: 9999, coinPrice: 20));
         }
 
 
         [Test]
-        public void Sell_WhenEnoughCoin_ShouldDecreaseCoinAndIncreaseUsdt()
+        public void Sell_WhenEnoughBtc_DecreasesBtcAndIncreasesUsdt()
         {
-            // Arrange
-            decimal usdtAmount = 0;
-
-            decimal expectedUsdtAfterSell = 20;
-            decimal expectedCoinAmount = 0;
-
-            var usdt = WalletItemMother.CreateUsdt(amount: usdtAmount);
+            var usdt = WalletItemMother.CreateUsdt(amount: 0);
             var btc = WalletItemMother.CreateBtc(amount: 1);
 
-            // Act
-            walletDomainService.Sell(usdt, btc, 1, BTC_PRICE);
+            walletDomainService.Sell(usdt, btc, amount: 1, coinPrice: 20);
 
-            // Assert
-            Assert.AreEqual(expectedUsdtAfterSell, usdt.Amount);
-            Assert.AreEqual(expectedCoinAmount, btc.Amount);
+            Assert.AreEqual(20, usdt.Amount);
+            Assert.AreEqual(0, btc.Amount);
         }
+
 
 
         [Test]
         public void Sell_WhenBtcIsInsufficient_ShouldThrowInsufficientFundsException()
         {
             // Arrange
-            decimal btcToSell = 1;
-
-            var usdt = WalletItemMother.CreateUsdt(amount: 100);
+            var usdt = WalletItemMother.CreateUsdt(amount: 0);
             var btc = WalletItemMother.CreateBtc(amount: 0);
 
             // Act and Assert
             Assert.Throws<Exceptions.InsufficientFundsException>(() =>
-            walletDomainService.Sell(usdt, btc, btcToSell, BTC_PRICE));
+            walletDomainService.Sell(usdt, btc, amount: 999, coinPrice: 20));
         }
 
     }
