44ee0c16a8b5440924692b87017081b04b146a94
Yuriy Balint <46222135+supalapka@users.noreply.github.com>
2026-02-09 19:11:11 +0700

    Created StakingDomainService and custom exception for staking

---
+++ CryptocurrencyExchange/Services/StakingDomain/IStakingDomainService.cs
@@ -0,0 +1,9 @@
+﻿using CryptocurrencyExchange.Models;
+
+namespace CryptocurrencyExchange.Services.StakingDomain
+{
+    public interface IStakingDomainService
+    {
+        Staking CreateStaking(WalletItem stakingCoinWalletItem, StakingCoin coinToStake, decimal amountToStake, int durationInMonth);
+    }
+}
---
+++ CryptocurrencyExchange/Services/StakingDomain/StakingDomainService.cs
@@ -0,0 +1,28 @@
+﻿using CryptocurrencyExchange.Exceptions;
+using CryptocurrencyExchange.Models;
+
+namespace CryptocurrencyExchange.Services.StakingDomain
+{
+    public class StakingDomainService : IStakingDomainService
+    {
+        public Staking CreateStaking(WalletItem stakingCoinWalletItem, StakingCoin cointToStake, decimal amountToStake, int durationInMonth)
+        {
+            if (stakingCoinWalletItem.Amount < amountToStake)
+                throw new InsufficientFundsException($"Not anough balance in {stakingCoinWalletItem.Symbol} to stake");
+
+            stakingCoinWalletItem.Amount -= amountToStake;
+
+            var stakingData = new Staking()
+            {
+                UserId = stakingCoinWalletItem.UserId,
+                StakingCoinId = cointToStake.Id,
+                Amount = amountToStake,
+                DurationInMonth = durationInMonth,
+                StartDate = DateTime.Today,
+                IsCompleted = false
+            };
+
+            return stakingData;
+        }
+    }
+}
