af91e35b25f88072f9b75ba011197eccb1942add
Yuriy Balint <46222135+supalapka@users.noreply.github.com>
2026-02-16 10:40:27 +0700

    Make WalletItem properties immutable from outside

--- CryptocurrencyExchange/Core/Entities/WalletItem.cs
+++ CryptocurrencyExchange/Core/Entities/WalletItem.cs
@@ -4,10 +4,10 @@ namespace CryptocurrencyExchange.Core.Models
 {
     public class WalletItem
     {
-        public int Id { get; set; }
-        public int UserId { get; set; }
-        public User User { get; set; }
-        public string Symbol { get; set; } = string.Empty;
+        public int Id { get; private set; }
+        public int UserId { get; private set; }
+        public User User { get; private set; }
+        public string Symbol { get; private set; } = string.Empty;
         public decimal Amount { get; private set; }
 
         private WalletItem() { } // for ORM
