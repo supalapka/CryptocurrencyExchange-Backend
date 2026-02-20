8a5d6ddad3c340193e313224b14c04b2c6385bbd
Yuriy Balint <46222135+supalapka@users.noreply.github.com>
2026-02-20 20:19:10 +0700

    Add crypto news update requester (via RabbitMQ)

---
+++ CryptocurrencyExchange/Infrastructure/News/CryptoNewsCrawlRequester.cs
@@ -0,0 +1,25 @@
+﻿using Crawler.Contracts;
+using CryptocurrencyExchange.Core.Interfaces.Services;
+using MassTransit;
+
+namespace CryptocurrencyExchange.Infrastructure.News
+{
+    public class CryptoNewsCrawlRequester : ICryptoNewsUpdateRequester
+    {
+        private readonly ISendEndpointProvider sendEndpointProvider;
+
+        public CryptoNewsCrawlRequester(ISendEndpointProvider sendEndpointProvider)
+        {
+            this.sendEndpointProvider = sendEndpointProvider;
+        }
+
+        public async Task TriggerUpdate(string coin)
+        {
+            var endpoint = await sendEndpointProvider.GetSendEndpoint(
+                new Uri("queue:crawler.start")
+            );
+
+            await endpoint.Send(new StartCrawl(coin));
+        }
+    }
+}
