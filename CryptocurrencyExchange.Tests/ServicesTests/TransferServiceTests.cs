d91208fe716d59ae5e2ea6a4f4ce75ddbd9be010
Yuriy Balint <46222135+supalapka@users.noreply.github.com>
2026-05-03 01:37:36 +0700

    Update TransferServiceTests to cover idempotency scenarios

--- CryptocurrencyExchange.Tests/ServicesTests/TransferServiceTests.cs
+++ CryptocurrencyExchange.Tests/ServicesTests/TransferServiceTests.cs
@@ -5,6 +5,8 @@ using CryptocurrencyExchange.Core.Models;
 using CryptocurrencyExchange.Core.ValueObject;
 using CryptocurrencyExchange.Core.ValueObject.User;
 using CryptocurrencyExchange.Exceptions;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging.Abstractions;
 using Moq;
 using NUnit.Framework;
@@ -19,11 +21,13 @@ namespace CryptocurrencyExchange.Tests.ServicesTests
         private Mock<IUserRepository> _userRepo;
         private Mock<IUnitOfWork> _uow;
         private Mock<ITransferVerificationOutboxRepository> _outboxRepo;
+        private Mock<ITransferIdempotentRequestRepository> _idempotentRepo;
 
         private TransferService _service;
 
         private const int SenderId = 1;
         private const int ReceiverId = 2;
+        private const string IdempotencyKey = "test-key-123";
         private static readonly Email SenderEmail = new("sender@test.com");
         private static readonly Email ReceiverEmail = new("receiver@test.com");
 
@@ -35,6 +39,7 @@ namespace CryptocurrencyExchange.Tests.ServicesTests
             _userRepo = new Mock<IUserRepository>();
             _uow = new Mock<IUnitOfWork>();
             _outboxRepo = new Mock<ITransferVerificationOutboxRepository>();
+            _idempotentRepo = new Mock<ITransferIdempotentRequestRepository>();
 
             _uow.Setup(x => x.ExecuteInTransactionAsync(It.IsAny<Func<Task>>()))
                 .Returns<Func<Task>>(f => f());
@@ -45,13 +50,52 @@ namespace CryptocurrencyExchange.Tests.ServicesTests
                 _userRepo.Object,
                 _uow.Object,
                 _outboxRepo.Object,
+                _idempotentRepo.Object,
                 NullLogger<TransferService>.Instance
             );
         }
 
         [Test]
-        public async Task InitiateAsync_ValidRequest_ShouldCreateTransferAndWriteOutboxEntry()
+        public async Task InitiateAsync_NewKey_ShouldCreateTransferAndWriteOutboxEntry()
         {
+            var receiver = new User(ReceiverEmail, new byte[] { 1 }, new byte[] { 2 });
+            var senderItem = new WalletItem(SenderId, CoinSymbol.Btc);
+            senderItem.AddAmount(10m);
+            SetUserProperty(senderItem, new User(SenderEmail, new byte[] { 3 }, new byte[] { 4 }));
+
+            _idempotentRepo.Setup(x => x.FindAsync(IdempotencyKey, SenderId)).ReturnsAsync((TransferIdempotentRequest)null);
+            _userRepo.Setup(x => x.GetByEmailAsync(ReceiverEmail)).ReturnsAsync(receiver);
+            _walletRepo.Setup(x => x.GetWithUserAsync(SenderId, CoinSymbol.Btc)).ReturnsAsync(senderItem);
+
+            var dto = new InitiateTransferDto(ReceiverEmail, "btc", 5m);
+            await _service.InitiateAsync(SenderId, dto, IdempotencyKey);
+
+            _transferRepo.Verify(x => x.AddAsync(It.IsAny<Transfer>()), Times.Once);
+            _outboxRepo.Verify(
+                x => x.AddAsync(It.Is<TransferVerificationOutbox>(e => e.Email == SenderEmail.Value)),
+                Times.Once);
+            _idempotentRepo.Verify(x => x.AddAsync(It.IsAny<TransferIdempotentRequest>()), Times.Once);
+        }
+
+        [Test]
+        public async Task InitiateAsync_ExistingValidKey_ShouldReturnCachedTransferIdWithoutCreatingTransfer()
+        {
+            var existingRequest = CreateIdempotentRequest(IdempotencyKey, SenderId, transferId: 42);
+            _idempotentRepo.Setup(x => x.FindAsync(IdempotencyKey, SenderId)).ReturnsAsync(existingRequest);
+
+            var dto = new InitiateTransferDto(ReceiverEmail, "btc", 5m);
+            var result = await _service.InitiateAsync(SenderId, dto, IdempotencyKey);
+
+            Assert.That(result, Is.EqualTo(42));
+            _transferRepo.Verify(x => x.AddAsync(It.IsAny<Transfer>()), Times.Never);
+            _outboxRepo.Verify(x => x.AddAsync(It.IsAny<TransferVerificationOutbox>()), Times.Never);
+        }
+
+        [Test]
+        public async Task InitiateAsync_ConcurrentConflict_ShouldReturnExistingTransferId()
+        {
+            _idempotentRepo.Setup(x => x.FindAsync(IdempotencyKey, SenderId)).ReturnsAsync((TransferIdempotentRequest)null);
+
             var receiver = new User(ReceiverEmail, new byte[] { 1 }, new byte[] { 2 });
             var senderItem = new WalletItem(SenderId, CoinSymbol.Btc);
             senderItem.AddAmount(10m);
@@ -60,36 +104,41 @@ namespace CryptocurrencyExchange.Tests.ServicesTests
             _userRepo.Setup(x => x.GetByEmailAsync(ReceiverEmail)).ReturnsAsync(receiver);
             _walletRepo.Setup(x => x.GetWithUserAsync(SenderId, CoinSymbol.Btc)).ReturnsAsync(senderItem);
 
-            var dto = new InitiateTransferDto(ReceiverEmail, "btc", 5m);
-            var transferId = await _service.InitiateAsync(SenderId, dto);
+            _uow.Setup(x => x.ExecuteInTransactionAsync(It.IsAny<Func<Task>>()))
+                .ThrowsAsync(new DbUpdateException("duplicate key", MakeDuplicateKeySqlException()));
 
-            _transferRepo.Verify(x => x.AddAsync(It.IsAny<Transfer>()), Times.Once);
-            _outboxRepo.Verify(
-                x => x.AddAsync(It.Is<TransferVerificationOutbox>(e => e.Email == SenderEmail.Value)),
-                Times.Once);
+            var conflictRecord = CreateIdempotentRequest(IdempotencyKey, SenderId, transferId: 99);
+            _idempotentRepo.Setup(x => x.FindAsync(IdempotencyKey, SenderId)).ReturnsAsync(conflictRecord);
+
+            var dto = new InitiateTransferDto(ReceiverEmail, "btc", 5m);
+            var result = await _service.InitiateAsync(SenderId, dto, IdempotencyKey);
+
+            Assert.That(result, Is.EqualTo(99));
         }
 
         [Test]
         public void InitiateAsync_ReceiverNotFound_ShouldThrowUserNotFoundException()
         {
+            _idempotentRepo.Setup(x => x.FindAsync(It.IsAny<string>(), It.IsAny<int>())).ReturnsAsync((TransferIdempotentRequest)null);
             _userRepo.Setup(x => x.GetByEmailAsync(It.IsAny<string>())).ReturnsAsync((User)null);
 
             var dto = new InitiateTransferDto("unknown@test.com", "btc", 5m);
 
             Assert.ThrowsAsync<UserNotFoundException>(async () =>
-                await _service.InitiateAsync(SenderId, dto));
+                await _service.InitiateAsync(SenderId, dto, IdempotencyKey));
         }
 
         [Test]
         public void InitiateAsync_SelfTransfer_ShouldThrowSelfTransferException()
         {
             var sender = new User(SenderEmail, new byte[] { 1 }, new byte[] { 2 });
+            _idempotentRepo.Setup(x => x.FindAsync(It.IsAny<string>(), It.IsAny<int>())).ReturnsAsync((TransferIdempotentRequest)null);
             _userRepo.Setup(x => x.GetByEmailAsync(SenderEmail)).ReturnsAsync(sender);
 
             var dto = new InitiateTransferDto(SenderEmail, "btc", 5m);
 
             Assert.ThrowsAsync<SelfTransferException>(async () =>
-                await _service.InitiateAsync(sender.Id, dto));
+                await _service.InitiateAsync(sender.Id, dto, IdempotencyKey));
         }
 
         [Test]
@@ -100,26 +149,28 @@ namespace CryptocurrencyExchange.Tests.ServicesTests
             senderItem.AddAmount(1m);
             SetUserProperty(senderItem, new User(SenderEmail, new byte[] { 3 }, new byte[] { 4 }));
 
+            _idempotentRepo.Setup(x => x.FindAsync(It.IsAny<string>(), It.IsAny<int>())).ReturnsAsync((TransferIdempotentRequest)null);
             _userRepo.Setup(x => x.GetByEmailAsync(ReceiverEmail)).ReturnsAsync(receiver);
             _walletRepo.Setup(x => x.GetWithUserAsync(SenderId, CoinSymbol.Btc)).ReturnsAsync(senderItem);
 
             var dto = new InitiateTransferDto(ReceiverEmail, "btc", 10m);
 
             Assert.ThrowsAsync<InsufficientFundsException>(async () =>
-                await _service.InitiateAsync(SenderId, dto));
+                await _service.InitiateAsync(SenderId, dto, IdempotencyKey));
         }
 
         [Test]
         public void InitiateAsync_SenderHasNoWalletItem_ShouldThrowWalletItemNotFoundException()
         {
             var receiver = new User(ReceiverEmail, new byte[] { 1 }, new byte[] { 2 });
+            _idempotentRepo.Setup(x => x.FindAsync(It.IsAny<string>(), It.IsAny<int>())).ReturnsAsync((TransferIdempotentRequest)null);
             _userRepo.Setup(x => x.GetByEmailAsync(ReceiverEmail)).ReturnsAsync(receiver);
             _walletRepo.Setup(x => x.GetWithUserAsync(SenderId, CoinSymbol.Btc)).ReturnsAsync((WalletItem)null);
 
             var dto = new InitiateTransferDto(ReceiverEmail, "btc", 5m);
 
             Assert.ThrowsAsync<WalletItemNotFoundException>(async () =>
-                await _service.InitiateAsync(SenderId, dto));
+                await _service.InitiateAsync(SenderId, dto, IdempotencyKey));
         }
 
         [Test]
@@ -142,10 +193,23 @@ namespace CryptocurrencyExchange.Tests.ServicesTests
         }
 
         [Test]
+        public async Task ConfirmAsync_AlreadyCompleted_ShouldReturnWithoutError()
+        {
+            var completed = Transfer.Create(SenderId, ReceiverId, CoinSymbol.Btc, 5m, "123456");
+
+            _transferRepo.Setup(x => x.GetPendingByIdAndSenderAsync(0, SenderId)).ReturnsAsync((Transfer)null);
+            _transferRepo.Setup(x => x.GetCompletedByIdAndSenderAsync(0, SenderId)).ReturnsAsync(completed);
+
+            var dto = new ConfirmTransferDto(0, "123456");
+
+            Assert.DoesNotThrowAsync(async () => await _service.ConfirmAsync(SenderId, dto));
+        }
+
+        [Test]
         public void ConfirmAsync_TransferNotFound_ShouldThrowTransferNotFoundException()
         {
-            _transferRepo.Setup(x => x.GetPendingByIdAndSenderAsync(99, SenderId))
-                .ReturnsAsync((Transfer)null);
+            _transferRepo.Setup(x => x.GetPendingByIdAndSenderAsync(99, SenderId)).ReturnsAsync((Transfer)null);
+            _transferRepo.Setup(x => x.GetCompletedByIdAndSenderAsync(99, SenderId)).ReturnsAsync((Transfer)null);
 
             var dto = new ConfirmTransferDto(99, "123456");
 
@@ -187,10 +251,28 @@ namespace CryptocurrencyExchange.Tests.ServicesTests
             Assert.That(senderItem.Amount.Value, Is.EqualTo(5m));
         }
 
+        private static TransferIdempotentRequest CreateIdempotentRequest(string key, int userId, int transferId)
+        {
+            var request = new TransferIdempotentRequest(key, userId);
+            request.SetTransferId(transferId);
+            return request;
+        }
+
         private static void SetUserProperty(WalletItem item, User user)
         {
             var prop = typeof(WalletItem).GetProperty("User");
             prop!.SetValue(item, user);
+        }
+
+        private static SqlException MakeDuplicateKeySqlException()
+        {
+            var flags = System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance;
+            var collection = (SqlErrorCollection)Activator.CreateInstance(typeof(SqlErrorCollection), nonPublic: true)!;
+            var errorCtor = typeof(SqlError).GetConstructors(flags).First(c => c.GetParameters().Length == 8);
+            var error = (SqlError)errorCtor.Invoke(new object?[] { 2627, (byte)0, (byte)0, "server", "duplicate key", "proc", 0, null });
+            typeof(SqlErrorCollection).GetMethod("Add", flags)!.Invoke(collection, new object[] { error });
+            return (SqlException)typeof(SqlException).GetConstructors(flags)[0]
+                .Invoke(new object?[] { "duplicate key", collection, null, Guid.NewGuid() });
         }
     }
 }
